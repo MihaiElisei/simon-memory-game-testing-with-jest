f354cfac5b21fab3d831aa6db18fafee37c0d737
MihaiElisei <mihai.m.elisei@gmail.com>
2022-10-15 16:16:43 +0000

    add showTurns function and test

--- scripts/game.js
+++ scripts/game.js
@@ -1,20 +1,45 @@
-const { getSystemErrorMap } = require("util")
-
 let game = {
-    score:0,
     currentGame: [],
     playerMoves: [],
+    score: 0,
+    turnNumber:0,
     choices: ["button1", "button2", "button3", "button4"],
-}
+};
 
-function newGame (){
-    game.score = 0;
-    game.playerMoves = [];
+function newGame() {
     game.currentGame = [];
-    showScore()
+    game.playerMoves = [];
+    game.score = 0;
+    showScore();
+    addTurn();
 }
 
-const showScore = () => document.getElementById("score").innerText = game.score
+function addTurn() {
+    game.playerMoves = [];
+    game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
+    showTurns();
+}
 
-module.exports = { game, newGame, showScore }
+function lightsOn(circ) {
+    document.getElementById(circ).classList.add(circ + "light");
+    setTimeout(function () {
+        document.getElementById(circ).classList.remove(circ + "light");
+    }, 400);
+}
 
+function showScore() {
+    document.getElementById("score").innerText = game.score;
+}
+
+function showTurns() {
+    game.turnNumber = 0;
+    let turns = setInterval(function () {
+        lightsOn(game.currentGame[game.turnNumber]);
+        game.turnNumber++;
+        if (game.turnNumber >= game.currentGame.length) {
+            clearInterval(turns);
+        }
+    }, 800);
+}
+
+module.exports = { game, newGame, showScore, addTurn, lightsOn, showTurns };
