9b9de5493f5a0317313565a1a48e5f5dce975d48
MihaiElisei <mihai.m.elisei@gmail.com>
2022-10-15 15:30:23 +0000

    add newGame and showScore functions and tests

--- scripts/game.js
+++ scripts/game.js
@@ -1,8 +1,20 @@
+const { getSystemErrorMap } = require("util")
+
 let game = {
     score:0,
-    currentGame:[],
-    playerMoves:[],
-    choices:[]
+    currentGame: [],
+    playerMoves: [],
+    choices: ["button1", "button2", "button3", "button4"],
 }
 
-module.exports = { game }
+function newGame (){
+    game.score = 0;
+    game.playerMoves = [];
+    game.currentGame = [];
+    showScore()
+}
+
+const showScore = () => document.getElementById("score").innerText = game.score
+
+module.exports = { game, newGame, showScore }
+
--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-const { game } = require("../game")
+const { game, newGame, showScore } = require("../game")
 
  beforeAll(() => {
     let fs = require("fs");
@@ -25,4 +25,29 @@ describe("game object contains correct keys", () => {
     test("choices key exists", () => {
         expect("choices" in game).toBe(true);
     });
+    test("choices contain the correct ids", () =>{
+        expect(game.choices).toEqual(["button1", "button2", "button3", "button4"])
+    });
 });
+
+describe("newGame works correctly", ()=> {
+    beforeAll(()=>{
+        game.score = 42;
+        game.playerMoves = ["button1", "button2"];
+        game.currentGame = ["button1", "button2"];
+        document.getElementById("score").innerText = "42"
+        newGame();
+    });
+    test("should set game score to zero", ()=>{
+        expect(game.score).toEqual(0)
+    });
+    test("playerMoves array should be empty" , ()=>{
+        expect(game.playerMoves.length).toBe(0)
+    })
+    test("Current game array should be empty", ()=>{
+        expect(game.currentGame.length).toBe(0)
+    })
+    test("Should display zero with the element with id of score", ()=>{
+        expect(document.getElementById("score").innerText).toEqual(0)
+    })
+})
